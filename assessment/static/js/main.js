c99f87f8dbadc44ef9ff115d6f900523e1287739
Your Name <you@example.com>
2018-06-13 14:54:51 +0500

    9

--- assessment/static/js/main.js
+++ assessment/static/js/main.js
@@ -6,7 +6,7 @@ if (element) {
       distance -= 10;
       if (distance < 0) {
         clearInterval(x);
-        element.innerHTML = "Vaqt Tugadi";
+        element.innerHTML = _["Vaqt Tugadi"];
       }
     }, 10000);
 }
@@ -14,7 +14,7 @@ function timeToString(time) {
     if (time>0){
         var hours = Math.floor(time / 3600);
         var minutes = Math.floor(time / 60) % 60;
-        return (hours ? hours + " soat ":"") + minutes + " daqiqa";
+        return (hours ? hours + _[" soat "]:"") + minutes + _[" daqiqa"];
     } else {
         return;
     }
@@ -23,7 +23,7 @@ function check() {
     var checkboxes = document.querySelectorAll('input[type="checkbox"], input[type="radio"');
     var checkedOne = Array.prototype.slice.call(checkboxes).some(x => x.checked);
     if (!checkedOne){
-        document.getElementById('valErr').innerHTML = "Iltimos Javobingizni tanlang"
+        document.getElementById('valErr').innerHTML = _["Iltimos Javobingizni tanlang"];
         return false;
     }
     return true;
@@ -32,7 +32,11 @@ function check() {
 function setTimeLeft(){
 
     bar.style.width = Math.min(1-distance/testTime, 1) * 100 + "%";
-    element.innerHTML = timeToString(distance) ? timeToString(distance) + " vaqtingiz qoldi" : "Vaqt Tugadi";
+    element.innerHTML = timeToString(distance) ? _[`$(timeToString(distance)) vaqtingiz qoldi] : _["Vaqt Tugadi"];
 }
 
 setTimeLeft()
+
+var _ = {
+
+}
--- core/settings.py
+++ core/settings.py
@@ -141,10 +141,11 @@ LANGUAGES = (
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
+STATIC_URL = '/stat/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, "static"),
+# )
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
